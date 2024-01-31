c765b672da76d85a52bf68d661f01911eae57b26
H3rzius <h3rzius@gmail.com>
2024-01-31 10:25:12 +0100

    edit routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,12 @@ import {EmployeeListComponent} from "./employee-list/employee-list.component";
 import {AuthGuard} from "./app.auth";
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/employees', pathMatch: 'full'},
-  { path: 'employees', component: EmployeeListComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/employee', pathMatch: 'full'},
+  { path: 'employee', component: EmployeeListComponent, canActivate: [AuthGuard] },
+  //{ path: 'employee/editor', component: EmployeeEditorComponent, canActivate: [AuthGuard] },
+  //{ path: 'employee/editor/:id', component: EmployeeEditorComponent, canActivate: [AuthGuard] },
+  //{ path: 'employee/:id', component: EmployeeDetailsComponent, canActivate: [AuthGuard] },
+  //{ path: 'qualification/editor', component: QualificationEditorComponent, canActivate: [AuthGuard] },
+  //{ path: 'qualification/:id', component: QualificationDetailsComponent, canActivate: [AuthGuard] },
+  //{ path: 'qualification', component: QualificationListComponent, canActivate: [AuthGuard] },
 ];
