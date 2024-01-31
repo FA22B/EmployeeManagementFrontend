d6dd833c25ded4b42dc581e594f259aa6598cf31
H3rzius <h3rzius@gmail.com>
2024-01-31 08:30:36 +0100

    Add authentificator

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),importProvidersFrom(KeycloakAngularModule),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    }]
 };
+
+export function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'https://keycloak.szut.dev/auth',
+        realm: 'szut',
+        clientId: 'employee-management-service-frontend'
+      },
+      initOptions: {
+        checkLoginIframe: true,
+        checkLoginIframeInterval: 25
+      }
+    });
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {EmployeeListComponent} from "./employee-list/employee-list.component";
+import {AuthGuard} from "./app.auth";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/employees', pathMatch: 'full'},
+  { path: 'employees', component: EmployeeListComponent, canActivate: [AuthGuard] },
+];
